8689f68847bb7238040d0fd0b8c78450abf93b06
An Bin <quocthienhong@gmail.com>
2024-02-13 16:02:47 +0700

    push store

---
+++ src/store/index.ts
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import { rootReducer } from "./rootReducer";
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+  }
+}
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
---
+++ src/store/rootReducer.ts
@@ -0,0 +1,23 @@
+import { combineReducers } from "redux";
+import courseListReducer from "../page/HomeTemplate/HomePage/CourseList/duck/reducer";
+import courseCategoryReducer from "../page/HomeTemplate/Course/duck/reducer";
+import categoryReducer from "../page/HomeTemplate/Category/duck/reducer";
+import authReducer from "page/AdminTemplate/AuthPage/duck/reducer";
+import userReducer from "page/AdminTemplate/ManageUser/duck/reducer";
+import detaiProductReducer from "page/HomeTemplate/Course/DetailProduct/duck/reducer";
+import productReducer from "page/AdminTemplate/ManageProduct/duck/reducer";
+import detailUserReducer from "page/HomeTemplate/Information/duck/reducer";
+import registedCourseReducer from "page/AdminTemplate/ManageUser/Course/duck/reducer";
+import UpdateDetailUserReducer from "page/HomeTemplate/Information/EditInfor/duck/reducer";
+export const rootReducer = combineReducers({
+  courseListReducer,
+  courseCategoryReducer,
+  categoryReducer,
+  authReducer,
+  userReducer,
+  detaiProductReducer,
+  productReducer,
+  detailUserReducer,
+  registedCourseReducer,
+  UpdateDetailUserReducer,
+});
